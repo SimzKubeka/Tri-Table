270095cda1d79cd0e34c06d8bef3aaa65939f199
SimsKubeka <sims.kubeka96@gmail.com>
2019-09-19 10:57:48 +0200

    Create TriTable.py
    Uing Python to create tri-table

---
+++ TriTable.py
@@ -0,0 +1,19 @@
+# Introduction to Programming: Task 15
+#THursday 23 May
+#Using Nested Statements
+
+print("Hi There!\n")
+
+
+for x in range(1, 10):
+    for y in range(1, x+1):
+        print(x*y, end = "  ")
+    print()
+
+
+
+
+print("\n     -------------")
+print("el alto colectivo")
+print("     -------------\n")
+
